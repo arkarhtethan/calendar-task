951b43b3bc0d913f35bf8210ad4956c5be2c82be
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-15 07:41:17 +0630

    calendar cell component added

---
+++ src/components/CalendarCell.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect, useState } from "react";
+import { CalendarContext } from "../context/CalendarContext";
+
+const CalendarCell = ({ day }) => {
+  const [className, setClassName] = useState(["days__grid--item"]);
+  const [notes, setNotes] = useState([]);
+  const { data } = useContext(CalendarContext);
+
+  useEffect(() => {
+    if (data[day?.format("D/M/YYYY")]) {
+      const { classes, notes } = data[day?.format("D/M/YYYY")];
+      if (classes) {
+        setClassName((prev) => [...prev, classes]);
+      }
+      if (notes) {
+        setNotes(notes);
+      }
+    }
+  }, []);
+  console.log(className);
+  return (
+    <div className={className.join(" ")}>
+      <p>{day?.format("D")}</p>
+      {notes.map((note, index) => (
+        <p key={index}>{note}</p>
+      ))}
+    </div>
+  );
+};
+
+export default CalendarCell;
