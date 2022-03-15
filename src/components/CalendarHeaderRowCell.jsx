cc85a5b41e3ed98e511e3ac4970c8291ea62d01e
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-15 07:41:57 +0630

    calendar row cell componmet added

---
+++ src/components/CalendarHeaderRowCell.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CalendarHeaderRowCell = ({ label }) => {
+  return <div className="calendar__row--header-cell">{label}</div>;
+};
+
+export default CalendarHeaderRowCell;
