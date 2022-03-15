e28e6a7a0eba2c462dac46fe1efd6f4dd31b8ab9
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-15 07:42:25 +0630

    calendar header added

---
+++ src/components/CalendarHeader.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const CalendarHeaderContainer = ({ children }) => (
+  <div className="calendar__header">{children}</div>
+);
+
+const CalendarHeaderLeft = ({ children }) => (
+  <div className="calendar__header--left">{children}</div>
+);
+
+const CalendarHeaderRight = ({ children }) => (
+  <div className="calendar__header--right">{children}</div>
+);
+const CalendarHeaderRightItem = ({ children, isActive = false }) => (
+  <div
+    className={`calendar__header--right__item ${
+      isActive && "calendar__header--right__item-active"
+    }`}
+  >
+    {children}
+  </div>
+);
+
+const CalendarHeader = () => {
+  return (
+    <CalendarHeaderContainer>
+      <CalendarHeaderLeft>
+        <p>&LeftArrow;</p>
+        <p>February, 2022</p>
+        <p>&RightArrow;</p>
+      </CalendarHeaderLeft>
+      <CalendarHeaderRight>
+        <CalendarHeaderRightItem isActive={true}>
+          <p>Month</p>
+        </CalendarHeaderRightItem>
+        <CalendarHeaderRightItem className="calendar__header--right__item">
+          <p>Week</p>
+        </CalendarHeaderRightItem>
+        <CalendarHeaderRightItem className="calendar__header--right__item">
+          <p>Day</p>
+        </CalendarHeaderRightItem>
+        <CalendarHeaderRightItem className="calendar__header--right__item">
+          <p>List</p>
+        </CalendarHeaderRightItem>
+      </CalendarHeaderRight>
+    </CalendarHeaderContainer>
+  );
+};
+
+export default CalendarHeader;
