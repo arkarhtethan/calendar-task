46843153aa232c98d35a4ff927416d7977b5face
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-16 16:20:17 +0630

    month grid added

---
+++ src/components/MonthDaysGrid.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { CalendarContext } from "../context/CalendarContext";
+import CalendarCell from "./CalendarCell";
+import FAB from "./FAB";
+
+const DaysGridContainer = ({ children }) => (
+  <div className="days__grid">{children}</div>
+);
+
+const MonthDaysGrid = () => {
+  const { calendar } = useContext(CalendarContext);
+  return (
+    <DaysGridContainer>
+      {calendar.map((weeks) =>
+        weeks.map((day) => (
+          <CalendarCell key={day.format("DD/MMMM/YYYY")} day={day} />
+        ))
+      )}
+      <FAB />
+    </DaysGridContainer>
+  );
+};
+
+export default MonthDaysGrid;
