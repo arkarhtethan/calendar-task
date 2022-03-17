82864533a261fc88361df6b3b3671146339e50f4
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-17 15:39:08 +0630

    move calendarcontextprovider and import style file form styles/index.scss

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import CalendarContextProvider from "./context/CalendarContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarContextProvider>
+      <App />
+    </CalendarContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
