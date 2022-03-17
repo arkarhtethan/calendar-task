707c7eb861507615be3f32d1bd127fc0578f8e38
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-17 18:06:45 +0630

    show notes on day grid

--- src/components/DayGrid.jsx
+++ src/components/DayGrid.jsx
@@ -1,3 +1,4 @@
+import { ClockCircleOutlined } from "@ant-design/icons";
 import React, { useContext } from "react";
 import { CalendarContext } from "../context/CalendarContext";
 
@@ -13,14 +14,44 @@ const DaysGridBody = ({ children }) => (
   <div className="days__grid--body">{children}</div>
 );
 
-const DaysGridItem = ({ children, index }) => (
-  <div className="days__grid--item">
-    <p className="days__grid--item__text-hour">
-      {index === 0 ? "All Day" : `${index} : 00`}
-    </p>
-    <p className="days__grid--item__text-event">{}</p>
-  </div>
-);
+const DaysGridItem = ({ children, hour }) => {
+  const { data, value } = useContext(CalendarContext);
+  const extractedData = data[value?.format("D/M/YYYY")];
+  const noteTime = false;
+  const notes = extractedData && extractedData.notes ? extractedData.notes : [];
+  return (
+    <div className="days__grid--item">
+      <p className="days__grid--item__text-hour">
+        {hour === 0 ? "All Day" : `${hour} : 00`}
+      </p>
+      {notes && !noteTime && hour === 0 && (
+        <div className="days__grid--item__text-event flex jcsb aic">
+          <div>
+            <div className="flex">
+              {notes &&
+                !noteTime &&
+                hour === 0 &&
+                notes?.map((note) => (
+                  <p style={{ fontSize: "12px" }}>{note}</p>
+                ))}
+            </div>
+            <div className="flex aic jcc">
+              <p className="list__item--date-icon">
+                <ClockCircleOutlined />
+              </p>
+              <p className="list__item--date-text">
+                {value.format("dddd , D MMMM , YYYY")}
+              </p>
+            </div>
+          </div>
+          <div>
+            <p style={{ fontSize: "12px" }}> All Day</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const DayGrid = () => {
   const { value } = useContext(CalendarContext);
@@ -33,7 +64,7 @@ const DayGrid = () => {
         {Array(25)
           .fill(0)
           .map((_, index) => (
-            <DaysGridItem key={index} index={index}>
+            <DaysGridItem key={index} hour={index}>
               {}
             </DaysGridItem>
           ))}
