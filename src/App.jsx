37147e84c3a6a9b11813cb08d8ab707bee1bd48b
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-17 15:26:37 +0630

    render grid conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import CalendarHeaderRow from "./components/CalendarHeaderRow";
-import DaysGrid from "./components/DaysGrid";
-import CalendarContextProvider from "./context/CalendarContext";
+import MonthDaysGrid from "./components/MonthDaysGrid";
+import { CalendarContext } from "./context/CalendarContext";
 import CalendarHeader from "./components/CalendarHeader";
+import { useContext } from "react";
+import WeekDaysGrid from "./components/WeekDaysGrid";
+import DayGrid from "./components/DayGrid";
+import ListGrid from "./components/ListGrid";
 
 function App() {
+  const { gridType } = useContext(CalendarContext);
+  const getGrid = () => {
+    if (gridType === "month") {
+      return <MonthDaysGrid />;
+    } else if (gridType === "week") {
+      return <WeekDaysGrid />;
+    } else if (gridType === "list") {
+      return <ListGrid />;
+    }
+    return <DayGrid />;
+  };
   return (
-    <CalendarContextProvider>
-      <div className="calendar">
-        <CalendarHeader />
-        <div className="calendar__main">
-          <CalendarHeaderRow />
-          <DaysGrid />
-        </div>
-      </div>
-    </CalendarContextProvider>
+    <div className="calendar">
+      <CalendarHeader />
+      <div className="calendar__main">{getGrid()}</div>
+    </div>
   );
 }
 
