68ded83ea509a837c2cb6b9161b1d19e7163ca7c
Kaung Myat Han <arkarhtethan@gmail.com>
2022-03-17 15:26:51 +0630

    moved to calendarheaderrow

--- src/components/CalendarHeaderRow.jsx
+++ src/components/CalendarHeaderRow.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import CalendarHeaderRowCell from "./CalendarHeaderRowCell";
 
 const CalendarHeaderRowContainer = ({ children }) => (
   <div className="calendar__row--header">{children}</div>
 );
-
-const CalendarHeaderRow = () => {
+const CalendarHeaderRowCell = ({ label }) => {
+  return <div className="calendar__row--header-cell">{label}</div>;
+};
+const CalendarHeaderRow = ({ children }) => {
   return (
     <CalendarHeaderRowContainer>
-      <CalendarHeaderRowCell label="Sun" />
-      <CalendarHeaderRowCell label="Mon" />
-      <CalendarHeaderRowCell label="Tue" />
-      <CalendarHeaderRowCell label="Wed" />
-      <CalendarHeaderRowCell label="Thu" />
-      <CalendarHeaderRowCell label="Fri" />
-      <CalendarHeaderRowCell label="Sat" />
+      {children ? (
+        <>{children}</>
+      ) : (
+        <>
+          <CalendarHeaderRowCell label="Sun" />
+          <CalendarHeaderRowCell label="Mon" />
+          <CalendarHeaderRowCell label="Tue" />
+          <CalendarHeaderRowCell label="Wed" />
+          <CalendarHeaderRowCell label="Thu" />
+          <CalendarHeaderRowCell label="Fri" />
+          <CalendarHeaderRowCell label="Sat" />
+        </>
+      )}
     </CalendarHeaderRowContainer>
   );
 };
--- src/components/CalendarHeaderRowCell.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CalendarHeaderRowCell = ({ label }) => {
-  return <div className="calendar__row--header-cell">{label}</div>;
-};
-
-export default CalendarHeaderRowCell;
